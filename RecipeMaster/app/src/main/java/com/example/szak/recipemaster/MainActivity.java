a3db0a28a5766606284c100e9c5cf4268a528413
Lukasz Zdrenka <szakulys@gmail.com>
2015-12-09 05:52:21 +0100

    Zmiana koloru fab

--- RecipeMaster/app/src/main/java/com/example/szak/recipemaster/MainActivity.java
+++ RecipeMaster/app/src/main/java/com/example/szak/recipemaster/MainActivity.java
@@ -10,10 +10,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton menuButton = (FloatingActionButton) findViewById(R.id.menuButton);
+        menuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
