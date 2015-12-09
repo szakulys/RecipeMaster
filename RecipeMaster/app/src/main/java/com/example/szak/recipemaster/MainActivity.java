a8727de8c19fade21e116f51dd0c606e50a02872
Lukasz Zdrenka <szakulys@gmail.com>
2015-12-09 19:29:46 +0100

    Poprawki ekranu 1

--- RecipeMaster/app/src/main/java/com/example/szak/recipemaster/MainActivity.java
+++ RecipeMaster/app/src/main/java/com/example/szak/recipemaster/MainActivity.java
@@ -18,8 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
 
         FloatingActionButton menuButton = (FloatingActionButton) findViewById(R.id.menuButton);
         menuButton.setOnClickListener(new View.OnClickListener() {
