6e01fe0bac288345bfee6e21c1195fb7634c9d60
Lukasz Zdrenka <szakulys@gmail.com>
2015-12-10 04:05:32 +0100

    Nowe guziki w menu i ustawienie ich widocznosci

--- RecipeMaster/app/src/main/java/com/example/szak/recipemaster/MainActivity.java
+++ RecipeMaster/app/src/main/java/com/example/szak/recipemaster/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.szak.recipemaster;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DialogTitle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -18,14 +22,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         FloatingActionButton menuButton = (FloatingActionButton) findViewById(R.id.menuButton);
+        final FloatingActionButton menuFacebookButton = (FloatingActionButton) findViewById(R.id.menuFacebookButton);;
+        final FloatingActionButton menuRecipeButton = (FloatingActionButton) findViewById(R.id.menuRecipeButton);
+        final DialogTitle recipeDialogTitle = (DialogTitle) findViewById(R.id.recipeDialogTitle);
+        final DialogTitle facebookDialogTitle = (DialogTitle) findViewById(R.id.facebookDialogTitle);
+
+
         menuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                menuFacebookButton.setVisibility(View.VISIBLE);
+                menuRecipeButton.setVisibility(View.VISIBLE);
+                recipeDialogTitle.setVisibility(View.VISIBLE);
+                facebookDialogTitle.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+
+        menuFacebookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        menuRecipeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
 
