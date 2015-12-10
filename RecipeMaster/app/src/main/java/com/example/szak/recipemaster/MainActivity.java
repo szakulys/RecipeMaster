9a830b51670776f6edbace0c9b8e10b39e753df9
Lukasz Zdrenka <szakulys@gmail.com>
2015-12-10 04:24:13 +0100

    Dodatkowy guzik X

--- RecipeMaster/app/src/main/java/com/example/szak/recipemaster/MainActivity.java
+++ RecipeMaster/app/src/main/java/com/example/szak/recipemaster/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DialogTitle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -22,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FloatingActionButton menuButton = (FloatingActionButton) findViewById(R.id.menuButton);
+        final FloatingActionButton menuButton = (FloatingActionButton) findViewById(R.id.menuButton);
+        final FloatingActionButton menuButtonX = (FloatingActionButton) findViewById(R.id.menuButtonX);
         final FloatingActionButton menuFacebookButton = (FloatingActionButton) findViewById(R.id.menuFacebookButton);;
         final FloatingActionButton menuRecipeButton = (FloatingActionButton) findViewById(R.id.menuRecipeButton);
         final DialogTitle recipeDialogTitle = (DialogTitle) findViewById(R.id.recipeDialogTitle);
@@ -37,6 +39,22 @@ public class MainActivity extends AppCompatActivity {
                 menuRecipeButton.setVisibility(View.VISIBLE);
                 recipeDialogTitle.setVisibility(View.VISIBLE);
                 facebookDialogTitle.setVisibility(View.VISIBLE);
+                menuButtonX.setVisibility(View.VISIBLE);
+                menuButton.setVisibility(View.GONE);
+
+            }
+        });
+
+        menuButtonX.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                menuFacebookButton.setVisibility(View.GONE);
+                menuRecipeButton.setVisibility(View.GONE);
+                recipeDialogTitle.setVisibility(View.GONE);
+                facebookDialogTitle.setVisibility(View.GONE);
+                menuButton.setVisibility(View.VISIBLE);
+                menuButtonX.setVisibility(View.GONE);
 
             }
         });
