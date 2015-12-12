816517a5d126fdd3feda5f49f798a0b6e0d17a88
Lukasz Zdrenka <szakulys@gmail.com>
2015-12-12 16:08:50 +0100

    LastCheckpoint

--- RecipeMaster/app/src/main/java/com/example/szak/recipemaster/MainActivity.java
+++ RecipeMaster/app/src/main/java/com/example/szak/recipemaster/MainActivity.java
@@ -1,42 +1,48 @@
 package com.example.szak.recipemaster;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.content.ContentResolver;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
-import android.content.res.Configuration;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.widget.ViewDragHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DialogTitle;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.text.Layout;
-import android.util.Base64;
-import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphRequestAsyncTask;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
-import java.io.Console;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
 
 
 public class MainActivity extends AppCompatActivity {
+ /*   private CallbackManager callbackManager;
+    private TextView facebookName;
+    Profile profile;
+    private ImageView facebookAvatar;
+    private FacebookCallback<LoginResult> callback;
+*/
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,24 +58,36 @@ public class MainActivity extends AppCompatActivity {
         final DialogTitle recipeDialogTitle = (DialogTitle) findViewById(R.id.recipeDialogTitle);
         final AppBarLayout appBar = (AppBarLayout) findViewById(R.id.appBar);
         final RelativeLayout contentMain = (RelativeLayout) findViewById(R.id.contentMain);
+  /*      menuFacebookButton.setReadPermissions("user_id");
+        callbackManager = CallbackManager.Factory.create();
+        menuFacebookButton.registerCallback(callbackManager, callback);
+        facebookName = (TextView) findViewById(R.id.facebookName);
+        facebookAvatar = (ImageView) findViewById(R.id.facebookAvatar);
 
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(
-                    "com.example.szak.recipemaster",
-                    PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+        callback = new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+                profile = Profile.getCurrentProfile();
+                if (profile != null){
+                    facebookName.setText("Logged as " + profile.getName());
+                    facebookAvatar.setImageURI(profile.getLinkUri());
+                }
+
             }
-        } catch (PackageManager.NameNotFoundException e) {
-
-        } catch (NoSuchAlgorithmException e) {
-
-        }
 
 
+            @Override
+            public void onCancel() {
 
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        };
+*/
         menuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -84,6 +102,8 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
 
         menuButtonX.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -110,21 +130,33 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        menuRecipeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            menuRecipeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
 
-                startActivity(new Intent(getApplicationContext(), PizzaRecipe.class));
+                    startActivity(new Intent(getApplicationContext(), PizzaRecipe.class));
 
-            }
-        });
+                }
 
-        Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(),R.drawable.pizza1);
-        Bitmap circularBitmap = ImageConverter.getRoundedCornerBitmap(bitmap, 100);
+            });
 
-        ImageView circularImageView = (ImageView)findViewById(R.id.circleView);
-        circularImageView.setImageBitmap(circularBitmap);
+
+
+            Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(), R.drawable.pizza1);
+            Bitmap circularBitmap = ImageConverter.getRoundedCornerBitmap(bitmap, 100);
+
+            ImageView circularImageView = (ImageView) findViewById(R.id.circleView);
+            circularImageView.setImageBitmap(circularBitmap);
+        }
+
+/*
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
+*/
+
 
 
 }
