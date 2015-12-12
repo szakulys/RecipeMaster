29fe3a38e5fc3c851c961953cfad4a4cac48a76e
Lukasz Zdrenka <szakulys@gmail.com>
2015-12-12 11:13:46 +0100

    Guzik facebooka juz dziala

--- RecipeMaster/app/src/main/java/com/example/szak/recipemaster/MainActivity.java
+++ RecipeMaster/app/src/main/java/com/example/szak/recipemaster/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.szak.recipemaster;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
@@ -15,6 +18,8 @@ import android.support.v7.widget.DialogTitle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.Layout;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,8 +29,11 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.facebook.FacebookSdk;
+import com.facebook.login.widget.LoginButton;
 
 import java.io.Console;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -39,12 +47,26 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         final FloatingActionButton menuButton = (FloatingActionButton) findViewById(R.id.menuButton);
         final FloatingActionButton menuButtonX = (FloatingActionButton) findViewById(R.id.menuButtonX);
-        final FloatingActionButton menuFacebookButton = (FloatingActionButton) findViewById(R.id.menuFacebookButton);;
+        final LoginButton menuFacebookButton = (LoginButton) findViewById(R.id.menuFacebookButton);;
         final FloatingActionButton menuRecipeButton = (FloatingActionButton) findViewById(R.id.menuRecipeButton);
         final DialogTitle recipeDialogTitle = (DialogTitle) findViewById(R.id.recipeDialogTitle);
-        final DialogTitle facebookDialogTitle = (DialogTitle) findViewById(R.id.facebookDialogTitle);
         final AppBarLayout appBar = (AppBarLayout) findViewById(R.id.appBar);
         final RelativeLayout contentMain = (RelativeLayout) findViewById(R.id.contentMain);
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.szak.recipemaster",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
 
 
 
@@ -55,7 +77,6 @@ public class MainActivity extends AppCompatActivity {
                 menuFacebookButton.setVisibility(View.VISIBLE);
                 menuRecipeButton.setVisibility(View.VISIBLE);
                 recipeDialogTitle.setVisibility(View.VISIBLE);
-                facebookDialogTitle.setVisibility(View.VISIBLE);
                 menuButtonX.setVisibility(View.VISIBLE);
                 menuButton.setVisibility(View.GONE);
                 appBar.setAlpha(0.4f);
@@ -71,7 +92,6 @@ public class MainActivity extends AppCompatActivity {
                 menuFacebookButton.setVisibility(View.GONE);
                 menuRecipeButton.setVisibility(View.GONE);
                 recipeDialogTitle.setVisibility(View.GONE);
-                facebookDialogTitle.setVisibility(View.GONE);
                 menuButton.setVisibility(View.VISIBLE);
                 menuButtonX.setVisibility(View.GONE);
                 appBar.setAlpha(1.0f);
@@ -84,6 +104,7 @@ public class MainActivity extends AppCompatActivity {
         menuFacebookButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
 
             }
         });
